133b5dd5b80be84736cb3bff7c19085a2d264166
Bekah Temple <bekahtemple@MacBooks-MBP.attlocal.net>
2023-08-07 19:11:08 -0500

    author info done, idk what ingredients is doing

--- part2/studio/src/components/AuthorInfo.js
+++ part2/studio/src/components/AuthorInfo.js
@@ -1,5 +1,33 @@
+import RecipeImage from "./RecipeImage";
+import recipedata from "./recipe.json";
+import styles from "./styling.css";
+
+
 function AuthorInfo() {
-   return;
+  
+  const recipeAuthor = recipedata.map((data) => (
+    <div key={data.name}>{data.author}</div>
+    ));
+    
+    const recipeAuthorImage = recipedata.map((data) => (
+      <div key={data.name}>
+        <img src={data.authorImage} alt={data.author} className="authorImage" />
+      </div>
+    ));
+    
+    const recipeWebsite = recipedata.map((data) => (
+    <div key={data.name}>
+      <a href={data.website}/>
+      </div>
+    ));
+
+   return (
+    <div>      
+      {recipeAuthorImage}
+      {recipeAuthor}
+      {recipeWebsite}
+    </div>
+   );
  }
  
  export default AuthorInfo;
