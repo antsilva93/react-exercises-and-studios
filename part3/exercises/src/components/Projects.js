6dbf7f779b8968540e3293613450ce3c81a10c5a
Bekah Temple <bekahtemple@MacBooks-MBP.attlocal.net>
2023-08-10 16:22:30 -0500

    done with exercise

--- part3/exercises/src/components/Projects.js
+++ part3/exercises/src/components/Projects.js
@@ -1,15 +1,27 @@
-import data from "../data.json";
-import {useState} from 'react';
+import data from './../data.json';
+import { useState } from 'react';
 
-const MyProjects() {
-    const [index, setIndex] useState(0);
+export default function MyProjects() {
+    const [index, setIndex] = useState(0);
+    let gameData = data;
+    let currentGame = gameData[index];
     function handleClick() {
-        if ( index < data.projects.length - 1) {
-            setIndex(index + 1)
+        if (index < gameData.length - 1) {
+            setIndex(index + 1);
         } else {
             setIndex(0);
         }
+        // console.log('click');
+
     }
+    return (
+        <div>
+            <button onClick={handleClick}>Next</button>
+            <div>Game Name: {currentGame.title}</div>
+            <div>Designer: {currentGame.designer}</div>
+            <img src={currentGame.photoURL} alt={currentGame.alt} />
+        </div>
+    )
 
 
 }
