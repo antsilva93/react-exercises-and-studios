64352651661ef2191895e16d5688bfad21ee2e2d
Bekah Temple <bekahtemple@MacBooks-MBP.attlocal.net>
2023-08-10 12:09:12 -0500

    json done

---
+++ part3/exercises/src/components/Introduction.js
@@ -0,0 +1,12 @@
+export default function HobbyIntroduction () {
+    return (
+        <div>
+        <h2>3 Fun Facts About this Hobby</h2>
+        <ol>
+            <li>over 5000 Board Games are released every year</li>
+            <li>The longest recorded game of Diplomacy took 3 Years, 7 months, and 10 days to finish</li>
+            <li>The earliest known board game, Ur, is roughly 5000 years old</li>
+        </ol>
+        </div>
+    );
+}
