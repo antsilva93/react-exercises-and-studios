fa506df3c97faf0759b6be6ab1ad83d4a238258a
Bekah Temple <bekahtemple@MacBooks-MBP.attlocal.net>
2023-08-10 15:37:45 -0500

    done with exercises(?)

---
+++ part3/exercises/src/components/Projects.js
@@ -0,0 +1,15 @@
+import data from "../data.json";
+import {useState} from 'react';
+
+const MyProjects() {
+    const [index, setIndex] useState(0);
+    function handleClick() {
+        if ( index < data.projects.length - 1) {
+            setIndex(index + 1)
+        } else {
+            setIndex(0);
+        }
+    }
+
+
+}
