ef7c20fb0f14c8734fe14622eabe2d02ffc69849
Bekah Temple <bekahtemple@MacBooks-MBP.attlocal.net>
2023-08-10 16:58:32 -0500

    boards done

--- part3/studio/src/components/BoardAssignment.js
+++ part3/studio/src/components/BoardAssignment.js
@@ -1,18 +1,38 @@
 import { useState } from 'react';
 
-export default function BoardAssignment () {
-   const boards = [];
+export default function BoardAssignment() {
+   const boards = [
+      {
+         label: "Happy Hour",
+         value: "Happy Hour"
+      },
+      {
+         label: "Fernet Fun",
+         value: "Fernet Fun"
+      },
+      {
+         label: "Equal Parts",
+         value: "Equal Parts"
+      }
+   ];
 
-   const handleChange = (event) => {
-   }
+const [boardName, setName] = useState('No Boards Yet');
 
-   return (
-      <div style={{paddingTop: "50px"}}>
+const handleChange = (event) => {
+   setName(event.target.value);
+}
+
+return (
+   <div style={{ paddingTop: "50px" }}>
       <label>Save to Board: </label>
       <select value={boardName} onChange={handleChange}>
+         {boards.map((board) => (
+         <option value={board.value}>{board.label}</option>
+         ))}
       </select>
 
       <p>Saved to {boardName}!</p>
-      </div>
-   );
+   </div>
+);
+
 }
